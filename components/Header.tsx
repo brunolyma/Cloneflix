a484b8284574c1eb66631bf3011fac2217f21068
Bruno Lima <brunoadl88@gmail.com>
2022-12-29 18:47:16 -0300

    cloneflix

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,23 @@
+import { BellIcon, SearchIcon } from "@heroicons/react/outline";
+
 import Image from "next/image";
+import Link from "next/link";
 import netflixLogo from "../public/netflix-logo.svg";
+import netflixProfile from "../public/netflix-profile.webp";
 
 export default function Header() {
   return (
     <header>
       <div className="flex items-center space-x-2 md:space-x-10">
-        <Image
-          src={netflixLogo}
-          alt="netflix logo"
-          width={100}
-          height={100}
-          className="cursor-pointer object-contain"
-        />
+        <Link href={"/"}>
+          <Image
+            src={netflixLogo}
+            alt="netflix logo"
+            width={100}
+            height={100}
+            className="cursor-pointer object-contain"
+          />
+        </Link>
 
         <ul className="hidden space-x-4 md:flex">
           <li className="headerlink">Home</li>
@@ -22,7 +28,18 @@ export default function Header() {
         </ul>
       </div>
 
-      <div></div>
+      <div className=" flex">
+        <SearchIcon className="hidden h-6 w-6 md:inline" />
+        <p className=" hidden lg:inline">Kids</p>
+        <BellIcon className=" h-6 w-6" />
+        <Link href={"/accounts"}>
+          <Image
+            src={netflixProfile}
+            alt="profile icon"
+            className="cursor-pointer rounded"
+          />
+        </Link>
+      </div>
     </header>
   );
 }
